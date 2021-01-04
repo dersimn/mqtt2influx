088079ff7e2f39549d9e6972d8cd70fb0504291f
Simon Christmann <simon@christmann.email>
2021-01-04 10:59:56 +0100

    implement queueing

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const config = require('yargs')
     .describe('influxdb-database')
     .describe('influxdb-measurement')
     .describe('subscription', 'array of topics to subscribe').array('subscription')
+    .describe('chunk-size', 'maximum number of points to buffer before writing to InfluxDB')
+    .describe('max-interval', 'maximum time to wait if chunk size is not completely filled before writing to InfluxDB anyway')
     .alias({
         h: 'help',
         m: 'mqtt-url',
+        i: 'influxdb-host',
         v: 'verbosity'
     })
     .default({
@@ -27,7 +30,9 @@ const config = require('yargs')
         'influxdb-measurement': 'data',
         subscription: [
             '#'
-        ]
+        ],
+        'chunk-size': 5000,
+        'max-interval': 3
     })
     .version()
     .help('help')
@@ -38,6 +43,8 @@ const Influx = require('influx');
 log.setLevel(config.verbosity);
 log.info(pkg.name + ' ' + pkg.version + ' starting');
 log.debug('loaded config: ', config);
+
+const pointBuffer = [];
 
 const mqttUrl = new URL(config.mqttUrl);
 log.debug('parsed mqttUrl: ', mqttUrl);
@@ -100,10 +107,46 @@ mqtt.on('message', (topic, message, packet) => {
         }
     };
 
-    // Write Datapoint
-    influx.writePoints([point]).then(() => {
-        log.debug('influx >', point.measurement);
-    }).catch(error => {
-        log.warn('influx >', point.measurement, error.message);
-    });
+    pointBuffer.push(point);
+
+    if (pointBuffer.length > config.chunkSize) {
+        setImmediate(write);
+    }
 });
+
+const writeInterval = setInterval(write, config.maxInterval * 1000);
+function write() {
+    const chunk = pointBuffer.splice(0, config.chunkSize);
+    if (chunk.length === 0) {
+        return;
+    }
+
+    // Write Datapoints
+    influx.writePoints(chunk).then(() => {
+        log.debug('influx >', chunk.length);
+    }).catch(error => {
+        log.warn('influx >', chunk.length, error.message);
+    });
+}
+
+process.on('SIGINT', () => {
+    log.info('received SIGINT');
+    stop();
+});
+process.on('SIGTERM', () => {
+    log.info('received SIGTERM');
+    stop();
+});
+async function stop() {
+    clearInterval(writeInterval);
+    mqtt.end();
+    try {
+        await influx.writePoints(pointBuffer);
+        log.debug('influx >', pointBuffer.length);
+    } catch (error) {
+        log.error('influx >', pointBuffer.length, error.message);
+    }
+
+    log.debug('exiting..');
+    process.exit(0);
+}
